474d59b1c29a697cae375c6939bbb9a868d3a9ee
belkadi <elharith.belkadi.etu@univ-lille.fr>
2025-12-04 08:32:48 +0000

    Troisième Commit : Dans le mainPanel, j'ai créé trois élément un text, un graph et un tableau

--- applicationexamenbelkadi/app.R
+++ applicationexamenbelkadi/app.R
@@ -12,30 +12,32 @@ ui <- fluidPage(
     # Application title
     titlePanel("Exploration des Diamants"),
     
-    # Sidebar with a slider input for number of bins 
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
           
-            radioButtons("rose", label = "Colorier les points en rose ?",
+            radioButtons(inputId="rose", label = "Colorier les points en rose ?",
                        choices = list("Oui" = 1, "Non" = 2), 
                        selected = 1),
             
-            selectInput("couleur", label = "Choisir une couleur à filtrer :", 
+            selectInput(inputId="couleur", label = "Choisir une couleur à filtrer :", 
                         choices = list("D" = "D", "E"="E", "F"="F", "G"="G", "H"="H", "I"="I", "J"="J"), 
                         selected = "D"),
             
-            sliderInput(inputId ="bins",
+            sliderInput(inputId ="prix",
                         label="Prix maximum :",
                         min = 300,
                         max = 20000,
-                        value = 30),
+                        value = 300),
             
             actionButton(inputId = "boutton", label = "Visualiser le graph")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          textOutput(outputId = "info"),
+          plotOutput("distPlot"),
+          DT::DTOutput(outputId = "tableau")
         )
     )
 )
@@ -44,14 +46,7 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+        
     })
 }
 
